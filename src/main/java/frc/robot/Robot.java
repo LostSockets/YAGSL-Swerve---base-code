7bdbdd171d50af4d9211cd067f4076fad5713717
MrCauthers <tv40670@gotvdsb.ca>
2025-02-03 14:50:34 -0500

    feb 3

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -74,6 +74,9 @@ public class Robot extends TimedRobot {
     // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
+    }else
+    {
+      CommandScheduler.getInstance().cancelAll();
     }
   }
 
